c603de26d660c1f1aa9a13a32cdc3a6f1458f16c
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-05-06 11:54:45 +0400

    auth

---
+++ Laravel/AuthentificationLaravel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = Auth::user();
+
+        // Auth::attempt(['email' => $email, 'password' => $password]);
+        // $posts = Post::all();
+        $posts = $user->posts->where();
+        // Post::where('user_id', Auth::id())->get();
+        return view('home')->with('user', $user)->with('posts', $posts);
+    }
+}
---
+++ Laravel/AuthentificationLaravel/app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function user() {
+        return $this->belongsTo(\App\User::class);
+    }
+
+    protected $fillable = [
+        'user_id',
+        'text'
+    ];
+}
